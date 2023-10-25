988daf9831acf82da06d645c438db1533f964a69
William Turner <willtur.will@gmail.com>
2023-10-24 22:45:26 -0400

    Fix Lua prelude code

--- luapack-core/src/prelude.lua
+++ luapack-core/src/prelude.lua
@@ -1,21 +1,30 @@
-local __luapack <const> = { preload = {}, loaded = {} }
+local __luapack<const> = {preload = {}, loaded = {}}
 
-local require <const> = function(modname)
-   local mod = __luapack.loaded[modname]
-   if mod ~= nil then
-      return mod
+setmetatable(__luapack.preload, {
+   __index = function(_t, k)
+      local v = _G[k]
+      if v ~= nil then
+         return function() return v end
+      else
+         return nil
+      end
    end
-   local loader = __luapack.modules[modname]
+})
+
+local require<const> = function(modname)
+   local mod = __luapack.loaded[modname]
+   if mod ~= nil then return mod end
+
+   local loader = __luapack.preload[modname]
    if load ~= nil then
       mod = loader(modname, ":preload:")
       if mod == nil then
          mod = __luapack.loaded[modname]
-         if mod == nil then
-            mod = true
-         end
+         if mod == nil then mod = true end
       end
       __luapack.loaded[modname] = mod
       return mod
    end
+
    error("module '" .. modname .. "' not found")
 end
